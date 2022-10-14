38b9839a57f98f0c2c51d8fe833ffdf8318b0b01
Ben Winchester <benedict.winchester@gmail.com>
2022-10-14 12:24:41 +0000

    :construction: Code plan in __main__

--- src/heatdesalination/__main__.py
+++ src/heatdesalination/__main__.py
@@ -16,3 +16,30 @@ This module ties together the functionality and code required in order to simula
 and optimise the desalination systems.
 
 """
+
+import sys
+
+from typing import Any, List
+
+def main(args: List[Any]) -> None:
+    """
+    Main module responsible for the flow of the HEATDesalination program.
+
+    Inputs:
+        - args:
+            The un-parsed command-line arguments.
+
+    """
+
+    # Parse the command-line arguments.
+    # Parse the various input files.
+
+    # IF: Simulation
+    #     THEN: Carry out a simulation based on the input conditions and exit.
+    # IF: Optimisation
+    #     THEN: Carry out an optimisation and exit.
+    # Raise an error stating that the code didn't run.
+    #
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
