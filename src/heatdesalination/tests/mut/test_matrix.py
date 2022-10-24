10c0e9d08ed5a849d6f7218cba24cca6e4d0c9f4
Ben Winchester <benedict.winchester@gmail.com>
2022-10-24 11:10:21 +0000

    :white_check_mark: Test collector-input-temperature function

--- src/heatdesalination/tests/mut/test_matrix.py
+++ src/heatdesalination/tests/mut/test_matrix.py
@@ -16,7 +16,9 @@ import unittest
 
 from unittest import mock
 
-from ..matrix import (
+from heatdesalination.__utils__ import HEAT_CAPACITY_OF_WATER
+
+from ...matrix import (
     _collectors_input_temperature,
     _solar_system_output_temperatures,
     _tank_temperature,
@@ -26,6 +28,36 @@ from ..matrix import (
 
 class TestCollectorsInputTemperature(unittest.TestCase):
     """Tests the `_collectors_input_temperature` helper function."""
+
+    def test_mainline(self) -> None:
+        """
+        Tests the mainline case.
+
+        The continuity equation being tested is the energy exchange through the heat
+        exchanger:
+
+        c_tank * eff_exchanger * (T_collector,out - T_tank)
+        = c_htf * (T_collector,out - T_collector,in)
+
+        """
+
+        collector_system_input_temperature = _collectors_input_temperature(
+            (collector_system_output_temperature := 65),
+            (heat_exchanger_efficiency := 0.4),
+            (htf_heat_capacity := HEAT_CAPACITY_OF_WATER),
+            (tank_temperature := 75),
+            (tank_water_heat_capacity := HEAT_CAPACITY_OF_WATER),
+        )
+
+        self.assertEqual(
+            tank_water_heat_capacity
+            * heat_exchanger_efficiency
+            * (collector_system_output_temperature - tank_temperature),
+            htf_heat_capacity
+            * (
+                collector_system_output_temperature - collector_system_input_temperature
+            ),
+        )
 
 
 class TestSolarSystemOutputTemperatures(unittest.TestCase):
